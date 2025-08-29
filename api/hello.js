003650f7425e9d0dedc33a04e0dab677bea0016b
Rizwan <rizwan@greenbankbristol.org>
2025-08-29 23:16:59 +0100

    chore: remove debug routes to stay under Vercel Hobby function limit

--- api/hello.js
+++
@@ -1,6 +0,0 @@
-export const config = { runtime: 'nodejs' };
-
-export default function handler(req, res) {
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  res.status(200).send(JSON.stringify({ ok: true, time: new Date().toISOString() }));
-}
--- api/push/blob-debug.js
+++
@@ -1,7 +0,0 @@
-// api/push/blob-debug.js
-export const config = { runtime: "nodejs" };
-
-export default async function handler(req, res) {
-  const hasToken = !!process.env.BLOB_READ_WRITE_TOKEN;
-  res.status(200).json({ ok: true, hasToken });
-}
